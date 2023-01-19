e81b1ee15fe1cae1fac395508940878a6c9bed18
tmdgus95 <8571053@naver.com>
2023-01-19 21:19:03 +0900

    상세 페이지 스타일링

--- src/components/ChannelInfo.jsx
+++ src/components/ChannelInfo.jsx
@@ -7,8 +7,9 @@ export default function ChannelInfo({ id, name }) {
     const { data: url } = useQuery(['channel', id], () => youtube.channelImageURL(id));
 
     return (
-        <div>
-            {url && <img src={url} alt={name}></img>} <p>{name}</p>
+        <div className='flex my-4 mb-8 items-center'>
+            {url && <img className='w-10 h-10 rounded-full' src={url} alt={name}></img>}{' '}
+            <p className='text-lg font-medium ml-2'>{name}</p>
         </div>
     );
 }
